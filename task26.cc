f23ddd8ea5a9081f1d59fd554976dceee89602a8
Shpikl-0 <kovtunovicvlad@gmail.com>
2025-05-09 09:38:22 +0300

    Function finder v.1.

--- task26.cc
+++ task26.cc
@@ -1,26 +1,54 @@
 #include <iostream>
 using namespace std;
 
+const int COLS = 9;
+const int ROWS = 9;
+
 const string COLOR = "\033[30;47m";
 const string RESET = "\033[0m";
 
+void finder(int matrix[ROWS][COLS])
+{
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            int value = matrix[i][j];
+
+            for(int k = 0; k < COLS; k++)
+            {
+                if(k != j && matrix[i][k] == value)
+                {
+                    matrix[i][k] +=10;
+                }
+            }
+        }
+    }
+}
+
+
 int main()
 {
-    int matrix[9][9] = {{1,1,4,3,4,1,3,2,2},
-                        {1,1,2,3,2,1,3,2,2},
-                        {3,2,1,4,3,3,2,1,3},
-                        {4,3,4,2,3,1,1,2,4},
-                        {4,2,1,1,2,3,3,4,1},
-                        {2,2,3,3,4,4,4,1,2},
-                        {2,3,3,1,3,2,2,4,1},
-                        {4,4,2,1,3,1,2,3,3},
-                        {4,4,2,1,1,1,2,3,3}};
+    int matrix[ROWS][COLS] = {{1,1,4,3,4,1,3,2,2},
+                              {1,1,2,3,2,1,3,2,2},
+                              {3,2,1,4,3,3,2,1,3},
+                              {4,3,4,2,3,1,1,2,4},
+                              {4,2,1,1,2,3,3,4,1},
+                              {2,2,3,3,4,4,4,1,2},
+                              {2,3,3,1,3,2,2,4,1},
+                              {4,4,2,1,3,1,2,3,3},
+                              {4,4,2,1,1,1,2,3,3}};
 
-    for(int i = 0; i < 9; i++)
+    finder(matrix);
+                
+    for(int i = 0; i < ROWS; i++)
     {
-        for(int j = 0; j < 9; j++)
-        {
-            cout << COLOR << matrix[i][j] << " " << RESET;
+        for(int j = 0; j < COLS; j++)
+        {   
+            if(matrix[i][j] > 10)
+                cout << COLOR << matrix[i][j] << RESET <<'\t';
+            else
+                cout << matrix[i][j] <<'\t';
         }
         cout << '\n';
     }
