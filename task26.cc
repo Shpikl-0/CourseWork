79517a4d9adfb1fe365399879d96947f329e2bfd
Shpikl-0 <kovtunovicvlad@gmail.com>
2025-05-09 10:03:14 +0300

    Functuon finder v.2, sort by rows

--- task26.cc
+++ task26.cc
@@ -7,25 +7,39 @@ const int ROWS = 9;
 const string COLOR = "\033[30;47m";
 const string RESET = "\033[0m";
 
-void finder(int matrix[ROWS][COLS])
+void finder(int matrix[ROWS][COLS],bool marks[ROWS][COLS])
 {
     for (int i = 0; i < ROWS; i++)
     {
+        bool visited[COLS] = {};
+
         for (int j = 0; j < COLS; j++)
         {
-            int value = matrix[i][j];
+            if(visited[j])
+                continue;
+
+            int count = 0;
+            int indices[COLS];
 
             for(int k = 0; k < COLS; k++)
             {
-                if(k != j && matrix[i][k] == value)
+                if(matrix[i][j] == matrix [i][k])
                 {
-                    matrix[i][k] +=10;
+                    indices[count++] = k;
+                    visited[k] = true;
+                }
+            }
+            
+            if(count > 1)
+            {
+                for (int x = 1; x < count; x++)
+                {
+                    marks[i][indices[x]] = true;
                 }
             }
         }
     }
 }
-
 
 int main()
 {
@@ -39,13 +53,15 @@ int main()
                               {4,4,2,1,3,1,2,3,3},
                               {4,4,2,1,1,1,2,3,3}};
 
-    finder(matrix);
+    bool marks[ROWS][COLS] = {};
+
+    finder(matrix, marks);
                 
     for(int i = 0; i < ROWS; i++)
     {
         for(int j = 0; j < COLS; j++)
         {   
-            if(matrix[i][j] > 10)
+            if(marks[i][j])
                 cout << COLOR << matrix[i][j] << RESET <<'\t';
             else
                 cout << matrix[i][j] <<'\t';
